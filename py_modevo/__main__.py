345c8f5b973def592abf2aef276069c7cbfabe7e
neelotpalnag <neelotpalnag@gmail.com>
2018-02-23 19:49:07 +0530

    Fix initialization method

--- py_modevo/__main__.py
+++ py_modevo/__main__.py
@@ -1,26 +1,22 @@
-import sys
-import random
-import math
-import cmath
-from . import initialization
+from initialization import initialize
 
 
 def main():
     # Parameters specific to Differential Evolution
-    population_size = 0
+    population_size = 4
     max_generations = 0
     num_ojectives = 0
     num_params = 0
 
-    X_hi = []
-    X_lo = []
+    X_hi = [6, 6]
+    X_lo = [0, 0]
 
     # Specify the Objective Functions in a Matrix format
     # For objective function matrix F(x) = [F1(x), F2(x) ... Fk(x)], where x = [x1, x2,x3 ..., xn]
     # F[0] = x[0] + x[1]
     # F[1] = x[0] - x[1]
 
-    X_init = initialization(X_hi, X_lo, population_size)
+    X_init = initialize(X_hi, X_lo, population_size)
     print(X_init)
 
 
--- py_modevo/initialization.py
+++ py_modevo/initialization.py
@@ -9,11 +9,11 @@
 import random
 
 
-def initialization(X_hi, X_lo, pop_size):
-    X_init = [[] * len(X_hi) for x in range(pop_size)]
+def initialize(X_hi, X_lo, pop_size):
+    X_init = [[0 for x in range(len(X_hi))] for y in range(pop_size)]
     for i in range(0, pop_size, 1):
         for j in range(0, len(X_hi), 1):
             rn = random.uniform(0, 1)
-            X_init[j][i] = X_lo[j] + rn * (X_hi[j] - X_lo[j])
+            X_init[i][j] = round(X_lo[j] + rn * (X_hi[j] - X_lo[j]), 4)
 
     return X_init
