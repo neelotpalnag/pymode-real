49957dedb78fa2708d748bc3a49a879a20ead4d9
Neelotpal Nag <neelotpalnag@gmail.com>
2018-03-22 00:11:21 +0530

    Change evaluation method

--- py_modevo/operations/selection.py
+++ py_modevo/operations/selection.py
@@ -1,13 +1,12 @@
-def selection(objF, X_trial, X_target, pop_size):
+from operations.evaluate import evaluate
+
+def selection(num_obj, X_trial, X_target, pop_size):
     X_sel = [[0 for x in range(len(X_trial[0]))] for y in range(pop_size)]
     for i in range(0, pop_size, 1):
         trial_wins = 0
-        for j in range(0, len(objF),1):
-            X = X_trial
-            trial_F_val = eval(objF[j])
-            X = X_target
-
-            target_F_val = eval(objF[j])
+        for j in range(0, num_obj,1):
+            trial_F_val = evaluate(j, X_trial[i])
+            target_F_val = evaluate(j, X_target[i])
             if trial_F_val < target_F_val:
                 trial_wins = 1
 
