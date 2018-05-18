72c477bd14262cc83cb861d8941868828c902fd5
Neelotpal Nag <neelotpalnag@gmail.com>
2018-05-18 23:46:15 +0530

    Restrict Objective evaluation in method

--- py_modevo/operations/evaluate.py
+++ py_modevo/operations/evaluate.py
@@ -5,13 +5,15 @@ def evaluate(obj_index, x_input):
     X = x_input
     F = [0, 0]
 
-    # Objective 1
-    F[0] = X[0]
+    if obj_index==0:
+        F[0] = X[0]
+        return F[0]
 
-    # Objective 2
-    F[1] = 91
-    for i in range(1, 10, 1):
-        F[1] = F[1] + pow(X[i],2) - (10 * cos(4 * pi * X[i]))
-    F[1] = F[1] * (1 - sqrt(X[0] / F[1]))
+    elif obj_index==1:
+        F[1] = 91
+        for i in range(1, 10, 1):
+            F[1] = F[1] + pow(X[i], 2) - (10 * cos(4 * pi * X[i]))
+        F[1] = F[1] * (1 - sqrt(X[0] / F[1]))
+        return F[1]
 
     return F[obj_index]
