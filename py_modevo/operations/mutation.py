382c5cf6083e493400e7d2f35279c67fd77fe5e3
Neelotpal Nag <neelotpalnag@gmail.com>
2018-05-18 11:44:52 +0530

    Implement Difference vector mode

--- py_modevo/operations/mutation.py
+++ py_modevo/operations/mutation.py
@@ -2,12 +2,13 @@ import random
 import math
 
 
-def mutate(X, X_hi, X_lo, pop_size, seed_gen):
+def mutate(X, X_hi, X_lo, pop_size, seed_gen, first_vector_type = 'rand', num_difference_vectors = 1):
     # The method "mutate" requires the following parameters:
     # X : The population from the previous generation
     # X_hi : Max. Values of Xi
     # X_lo : Min. Values of Xi
     # pop_size : Population Size
+    # num_difference_vectors : Number of Difference Vectors to be used
 
 
     num_vars = len(X_hi)
@@ -15,15 +16,49 @@ def mutate(X, X_hi, X_lo, pop_size, seed_gen):
     random.seed(seed_gen)
     F = 2*random.uniform(0,1)
 
-    for i in range(0, pop_size, 1):
-        for j in range(0, num_vars, 1):
-            rn_int = rn_int = random.sample(range(0, num_vars), 3)
-            val = X[i][rn_int[0]] + F * (X[i][rn_int[1]] - X[i][rn_int[2]])
-            if val > X_hi[j]:
-                X_mut[i][j] = X_hi[j]
-            elif val < X_lo[j]:
-                X_mut[i][j] = X_lo[j]
-            else:
-                X_mut[i][j] = val
+
+    # NUM_DIFFERENCE_VECTORS = 1 : One Difference Vector
+    # NUM_DIFFERENCE_VECTORS = 2 : Two Difference Vectors
+    if num_difference_vectors == 2:
+        num_rans = 5
+    else:
+        num_rans = 3
+
+    # FIRST_VECTOR_TYPE : 'rand' or 'best'
+    X_FIRST = []
+    # if first_vector_type == 'best':
+    #     X_FIRST =
+    # else:
+    #     pass
+
+
+    if num_rans == 3:
+        for i in range(0, pop_size, 1):
+            for j in range(0, num_vars, 1):
+
+                rn_int = random.sample(range(0, num_vars), num_rans)
+                val = X[i][rn_int[0]] + F * (X[i][rn_int[1]] - X[i][rn_int[2]])
+
+                if val > X_hi[j]:
+                    X_mut[i][j] = X_hi[j]
+                elif val < X_lo[j]:
+                    X_mut[i][j] = X_lo[j]
+                else:
+                    X_mut[i][j] = val
+
+
+    else:
+        for i in range(0, pop_size, 1):
+            for j in range(0, num_vars, 1):
+                rn_int = rn_int = random.sample(range(0, num_vars), num_rans)
+
+                val = X[i][rn_int[0]] + F * (X[i][rn_int[1]] - X[i][rn_int[2]]) + F * (X[i][rn_int[3]] - X[i][rn_int[4]])
+
+                if val > X_hi[j]:
+                    X_mut[i][j] = X_hi[j]
+                elif val < X_lo[j]:
+                    X_mut[i][j] = X_lo[j]
+                else:
+                    X_mut[i][j] = val
 
     return X_mut
